dfabf6a9da225cff1f425cf2c40c099bf2d226f3
chitochuks <fayveechuks@gmail.com>
2023-03-20 11:57:46 +0100

    first commit

---
+++ src/assets/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import css from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={css.container}>
+        <div className={css.logo}>
+            logo
+        </div>
+        <div className={css.right}>
+            <div className={css.menu}>
+
+            </div>
+            <input type="text" classNamr={css.search} />
+
+            <span>PNG icon of cart</span>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,35 @@
+import profile1 from '../assets/profile1.png'
+import profile2 from '../assets/profile2.png'
+
+export const TestimonialsData = [
+    {
+        image : profile1,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "John Abayomi"
+    },
+    {
+        image : profile2,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Lydia"
+    },
+    {
+        image : profile1,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Alex Chuks"
+    },
+    {
+        image : profile1,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Tonia Ofili"
+    },
+    {
+        image : profile2,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Natasha"
+    },
+    {
+        image : profile1,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Chito"
+    }
+]
